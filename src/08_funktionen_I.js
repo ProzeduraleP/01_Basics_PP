bf274748d34426d7c27370b8ee40a9e8c477e449
Arian Hormozfar <a.hormozfar@yahoo.de>
2024-07-04 09:40:36 +0000

    08 02 funktionen

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -1,4 +1,32 @@
 
+const prompt = require('prompt-sync')({sigint: true});
+
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
+
+test();
+
+function test() {
+    console.log("Funktion test");
+}
+
+/***** Funktionen 02a *****/
+// 2 Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen2(firstName = "Noch nicht bekannt") {
+    console.log("Hallo, " + firstName + "!");
+}
+
+ausgabeNamen2("Peter");
+
+/***** Funktionen 02a *****/
+// 3 Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen3(firstName, familyName) {
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+}
+
+ausgabeNamen3("Peter", "Schmit");
+
+ausgabeNamen3(prompt("Vorname? "), prompt("Name? "));
 
