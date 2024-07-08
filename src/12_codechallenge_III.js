f65133ba6a06f5cae2c12b7c64e287fedc8f7dda
Arian Hormozfar <a.hormozfar@yahoo.de>
2024-07-08 21:31:07 +0000

    12 / 02 code challenge

--- src/12_codechallenge_III.js
+++ src/12_codechallenge_III.js
@@ -1,5 +1,3 @@
-
-
 /*** Mini-Challenge  */
 // Satzbau + variable Punktuation
 
@@ -10,19 +8,50 @@
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
 
-
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
+const prompt = require('prompt-sync')({sigint: true});
+
+function isOpNotValid(arr) {
+
+    let i = 1;
+    let val = prompt("Punktation für \"" + arr.join(" ") + "\" wurde nicht automatisch erkannt! bitte Manuel eingeben: ");
+
+    while (val != "." && val && "?" && val != "!" && val != "") {
+
+        if (i < 3) {
+            i++;
+            val = prompt(i + ". Versuch: ");
+        } else {
+            val = "Kein Richtige eingabe! Bitte versuchen Sie es noch einmal!";
+            return val;
+        }
+
+    }
+    return arr.join(" ") + val;
+    
+}
+
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
+output(getSentence(["Ich","weiß","nicht","wie","soll","ich"],"F"));
+
 function getSentence(arr,op) {
-   // ???
+   switch (op) {
+    case "S":
+        return arr.join(" ") + ".";
+    case "Q":
+        return arr.join(" ") + "?";   
+    case "E":
+        return arr.join(" ") + "!";
+
+    default:
+        return isOpNotValid(arr);
+   }
 }
-
-
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
